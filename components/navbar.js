cc30332ef30a43edb4e50ebaa2aeec9be32bf942
Luciano Ribeiro <42896706+luciano44@users.noreply.github.com>
2022-06-23 18:01:31 -0300

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -23,6 +23,22 @@ function Navbar() {
       <Link href="/users">
         <a>users</a>
       </Link>
+      <br />
+      <Link href="/zzCSR">
+        <a>CSR</a>
+      </Link>
+       <br />
+      <Link href="/zzSSR">
+        <a>SSR</a>
+      </Link>
+       <br />
+      <Link href="/zzSSG">
+        <a>SSG</a>
+      </Link>
+       <br />
+      <Link href="/zzISR">
+        <a>ISR</a>
+      </Link>
       <hr />
     </div>
   );
